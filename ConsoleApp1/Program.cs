aa18355920dbb2d723891009c7559c542c68c9b9
Cameron White <camwhite1@outlook.com>
2021-07-30 01:36:16 -0400

    added getTimeDate() method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,81 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
 
-        
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Register!");
-            Console.WriteLine("Enter any key to continue.");
-            Console.Write("The current date and time is: ");
-            DateTime now = DateTime.Now;
-            Console.WriteLine(now);
-            Console.ReadLine();
             
-            Console.WriteLine("How many items are you Purchasing?");
-            int response = Convert.ToInt32(Console.ReadLine());
-            
-            int i = 0;
-            double total = 0;
-            while(i < response)
-            {
-                Console.WriteLine("Please enter price of " + i + " out of " + response);
-                double price = Convert.ToDouble(Console.ReadLine());
-                
-                total = total + price;
-                i++;
-                Console.WriteLine("Subtotal: $" + total);
-                
-            }
-            Console.WriteLine("===================================================");
-            Console.WriteLine("Subtotal: $" + total);
-            double tax = total * 0.06;
-            Console.WriteLine("6% Sales Tax: $" + tax);
-            double finalTotal = total + tax;
-            Console.WriteLine("Total: $" + finalTotal);
-            
-            Console.WriteLine("Do you have any coupons? (Y/N)");
-            String answer = Console.ReadLine();
-            int totalCouponAmount = 0;
-            if (answer == "Y")
-            {
-                Console.WriteLine("How many coupons do you have?");
-                int coupNum = Convert.ToInt32(Console.ReadLine());
-                int x = 0;
-                
-                while (x < coupNum)
-                {
-                    Console.WriteLine("Enter coupon   " + x + " amount: ");
-                    int coupTotal = Convert.ToInt32(Console.ReadLine());
-                    totalCouponAmount = totalCouponAmount + coupTotal;
-                    x++;
-                }
-                
-            }
-            else
-            {
-                Console.WriteLine("Continuing...");
-            }
-            
-            Console.WriteLine("===============================================");
-            Console.WriteLine("Discounts: $ " + totalCouponAmount);
-            double newSub = total - totalCouponAmount;
-            Console.WriteLine("Subtotal: $" + newSub);
-            double newTax = newSub * 0.06;
-            Console.WriteLine("Tax: $" + newTax);
-            double grandTotal = newTax + newSub;
-            Console.WriteLine("Grand Total: $" + grandTotal);
-            checkout(grandTotal);
+                Console.WriteLine("Welcome to the Register!");
+                Console.WriteLine("Enter any key to continue.");
+                GetDateTime();
 
-        }
+                Console.WriteLine("How many items are you Purchasing?");
+                int response = Convert.ToInt32(Console.ReadLine());
+
+                int i = 0;
+                double total = 0;
+                while (i < response)
+                {
+                    Console.WriteLine("Please enter price of " + i + " out of " + response);
+                    double price = Convert.ToDouble(Console.ReadLine());
+
+                    total = total + price;
+                    i++;
+                    Console.WriteLine("Subtotal: $" + total);
+
+                }
+
+                Console.WriteLine("===================================================");
+                Console.WriteLine("Subtotal: $" + total);
+                double tax = total * 0.06;
+                Console.WriteLine("6% Sales Tax: $" + tax);
+                double finalTotal = total + tax;
+                Console.WriteLine("Total: $" + finalTotal);
+
+                Console.WriteLine("Do you have any coupons? (Y/N)");
+                String answer = Console.ReadLine();
+                int totalCouponAmount = 0;
+                if (answer == "Y")
+                {
+                    Console.WriteLine("How many coupons do you have?");
+                    int coupNum = Convert.ToInt32(Console.ReadLine());
+                    int x = 0;
+
+                    while (x < coupNum)
+                    {
+                        Console.WriteLine("Enter coupon   " + x + " amount: ");
+                        int coupTotal = Convert.ToInt32(Console.ReadLine());
+                        totalCouponAmount = totalCouponAmount + coupTotal;
+                        x++;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Continuing...");
+                }
+
+                Console.WriteLine("===============================================");
+                Console.WriteLine("Discounts: $ " + totalCouponAmount);
+                double newSub = total - totalCouponAmount;
+                Console.WriteLine("Subtotal: $" + newSub);
+                double newTax = newSub * 0.06;
+                Console.WriteLine("Tax: $" + newTax);
+                double grandTotal = newTax + newSub;
+                Console.WriteLine("Grand Total: $" + grandTotal);
+                checkout(grandTotal);
+                
+            }
+        
 
         public static void checkout(double total)
         {
@@ -90,6 +86,7 @@ namespace ConsoleApp1
                 Console.WriteLine("Please follow instructions on pin pad.");
                 Console.WriteLine("Transacting $" + total);
                 Console.WriteLine("Transaction Complete!");
+                
             }
 
             if (checkoutResponse == 2)
@@ -105,7 +102,17 @@ namespace ConsoleApp1
                 Console.WriteLine("Enter $" + total);
                 Console.WriteLine("Transaction Complete!");
             }
+
+            
         }
+        public static void GetDateTime()
+        {
+            Console.Write("The current date and time is: ");
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.ReadLine();
+        }
+
         
     }
 }
